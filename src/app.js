5c53c1bf242acf0ee63bd6ce3d23c05c57fd261d
MatyMusic <mtyg7702@gmail.com>
2023-11-19 23:25:25 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+//create express app
+const app = express()
+
+
+app.post('/test', (req, res) => {
+    res.send('hello from server')
+})
+
+
+
+
+export default app
+
